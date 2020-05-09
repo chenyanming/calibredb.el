78965cfdf4d247f31a0ecf42e644f83fdb357bbd
Damon Chan <damonchan@DamondeiMac.local>
2020-05-10 01:48:49 +0800

    Add demos and README.

--- calibredb.el
+++ calibredb.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2020 Damon Chan
 
 ;; Author: Damon Chan <elecming@gmail.com>
-;; URL: https://github.com/chenyanming/calibredb
+;; URL: https://github.com/chenyanming/calibredb.el
 ;; Keywords: faces
 ;; Created: 9 May 2020
 ;; Version: 1.0
