a82143d534dd43f26a980714c4d0d8137cc8ea08
chenyanming <elecming@gmail.com>
2020-05-21 10:37:49 +0800

    Change the emacs required version.

--- calibredb.el
+++ calibredb.el
@@ -7,7 +7,7 @@
 ;; Keywords: tools
 ;; Created: 9 May 2020
 ;; Version: 1.5.0
-;; Package-Requires: ((emacs "25.1") (org "9.0") (transient "0.1.0"))
+;; Package-Requires: ((emacs "24.3") (org "9.0") (transient "0.1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
