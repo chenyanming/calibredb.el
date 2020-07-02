26757d9a04ca9d53170f5c474bfeab5d5dff6b4f
chenyanming <elecming@gmail.com>
2020-07-02 10:41:11 +0800

    Sort calibredb-add by date by default.

--- calibredb-search.el
+++ calibredb-search.el
@@ -299,6 +299,7 @@ Indicating the library you use."
   (buffer-disable-undo)
   (set (make-local-variable 'hl-line-face) 'calibredb-search-header-highlight-face)
   (hl-line-mode)
+  (add-to-list 'ivy-sort-matches-functions-alist '(calibredb-add . ivy--sort-files-by-date))
   (add-hook 'minibuffer-setup-hook 'calibredb-search--minibuffer-setup))
 
 (defun calibredb-search-mouse (event)
