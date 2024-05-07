8ca1b5262f0f0e4a0e295efc2c14db00333e9a85
chenyanming <elecming@gmail.com>
2024-05-07 12:56:08 +0800

    Add calibredb-consult-ripgrep-all (POC)

--- calibredb-consult.el
+++ calibredb-consult.el
@@ -33,6 +33,13 @@
 (eval-when-compile (defvar calibredb-search-entries))
 (eval-when-compile (defvar calibredb-full-entries))
 
+(defcustom calibredb-consult-ripgrep-all-args
+  "rga --null --line-buffered --color=never --max-columns=1000 --path-separator /\  --smart-case --no-heading --with-filename --line-number --rga-adapters=pandoc"
+  "Command line arguments for ripgrep, see `calibredb-consult-ripgrep-all'.
+The dynamically computed arguments are appended.
+Can be either a string, or a list of strings or expressions."
+  :type '(choice string (repeat (choice string expression))))
+
 (defun calibredb-consult-read (arg)
   "consult read for calibredb."
   (interactive "P")
@@ -48,6 +55,45 @@
                            :lookup #'consult--lookup-cdr
                            :sort nil) arg)
           (message "INVALID LIBRARY")))))
+
+
+(defun calibredb-consult--ripgrep-all-make-builder (paths)
+  "Create ripgrep command line builder given PATHS."
+  (let* ((cmd (consult--build-args calibredb-consult-ripgrep-all-args))
+         (type (if (consult--grep-lookahead-p (car cmd) "-P") 'pcre 'extended)))
+    (lambda (input)
+      (pcase-let* ((`(,arg . ,opts) (consult--command-split input))
+                   (flags (append cmd opts))
+                   (ignore-case
+                    (and (not (or (member "-s" flags) (member "--case-sensitive" flags)))
+                         (or (member "-i" flags) (member "--ignore-case" flags)
+                             (and (or (member "-S" flags) (member "--smart-case" flags))
+                                  (let (case-fold-search)
+                                    ;; Case insensitive if there are no uppercase letters
+                                    (not (string-match-p "[[:upper:]]" arg))))))))
+        (if (or (member "-F" flags) (member "--fixed-strings" flags))
+            (cons (append cmd (list "-e" arg) opts paths)
+                  (apply-partially #'consult--highlight-regexps
+                                   (list (regexp-quote arg)) ignore-case))
+          (pcase-let ((`(,re . ,hl) (funcall consult--regexp-compiler arg type ignore-case)))
+            (when re
+              (cons (append cmd (and (eq type 'pcre) '("-P"))
+                            (list "-e" (consult--join-regexps re type))
+                            opts paths)
+                    hl))))))))
+
+;;;###autoload
+(defun calibredb-consult-ripgrep-all (&optional dir initial)
+  "Search with `rga` for files in DIR where the content matches a regexp.
+  The initial input is given by the INITIAL argument. See `consult-grep`
+  for more details."
+  (interactive "P")
+  (if (fboundp 'consult--grep)
+      (let* ((result (consult--grep "Search Calibredb: " #'calibredb-consult--ripgrep-all-make-builder (or dir calibredb-root-dir) initial))
+             (parts (split-string result ":"))
+             (file-name (car parts)))
+        (when file-name
+          (find-file file-name))) ))
 
 (provide 'calibredb-consult)
 
