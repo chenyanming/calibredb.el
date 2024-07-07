63a775de992b2aee356f653e17128a2929b297c1
chenyanming <elecming@gmail.com>
2024-07-07 11:35:12 +0800

    Organize README

--- calibredb.el
+++ calibredb.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/chenyanming/calibredb.el
 ;; Keywords: tools
 ;; Created: 9 May 2020
-;; Version: 2.12.0
+;; Version: 2.13.0
 ;; Package-Requires: ((emacs "25.1") (org "9.3") (transient "0.1.0") (s "1.12.0") (dash "2.17.0") (request "0.3.3") (esxml "0.3.7"))
 
 ;; This file is NOT part of GNU Emacs.
